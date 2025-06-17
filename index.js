41ebe7c7922aa9c6ab409890b42888071eff9af3
unknown <nabpessoal@gmail.com>
2025-06-17 13:45:35 -0300

    Tipo de entrega

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
 const promptSync = require("prompt-sync")();
+// Tipo de entrega (normal ou urgente).
 
-console.log("oiii")
+let tipoEntrega = 0
 
+while (tipoEntrega == 0) {
+    tipoEntrega = promptSync("Digite o tipo de entrega (Normal ou Urgente): ")
+    if(tipoEntrega == "Normal"){
+        console.log("Você definiu o tipo da entrega como Normal")
+    }else{
+        if(tipoEntrega == "Urgente"){
+            console.log("Você definiu o tipo da entrega como Urgente")
+        }else{
+            tipoEntrega = 0
+            console.log("Erro: Tipo de entrega inválida.")
+        }
+    }
+}
